14a87650697b9e6cd03ab0e7bde201fb1c6f6a83
AmmarMasood <ammar.masood98@gmail.com>
2023-10-27 03:10:39 +0500

    fixed

--- src/app/lib/firebase-admin-config.ts
+++ src/app/lib/firebase-admin-config.ts
@@ -4,7 +4,9 @@ const firebaseAdminConfig = {
   credential: cert({
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY
+      ? process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+      : undefined,
   }),
 };
 
