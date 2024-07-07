8038d97e296e452354e9401824bce78cd404c713
AmmarMasood <ammar.masood98@gmail.com>
2024-07-07 23:34:28 +0500

    return error incase of prisma error

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -7,56 +7,7 @@ const prisma = new PrismaClient();
 
 const webhookSecret = process.env.NEXT_PUBLIC_STRIPE_WEBHOOK_SECRET as string;
 
-const handleAccountUpdated = async (event: Stripe.Event) => {
-  try {
-    console.log("Account Updated event received");
-    console.log("prisma", prisma);
-    const eventAccountUpdated = event as Stripe.AccountUpdatedEvent;
-    console.log("eventAccountUpdated", eventAccountUpdated);
-    // get the account ID from the event
-    const accountID = eventAccountUpdated.account as string;
-    console.log("accountID", accountID);
-    // get the account data from db
-    let user;
-    try {
-      console.log("user -1", user, prisma);
-      user = await prisma.userProfile.findUnique({
-        where: {
-          stripeAccountId: accountID,
-        } as any,
-      });
-      console.log("user 0", user);
-    } catch (dbError) {
-      console.error("Database query failed", dbError);
-      console.log("Database query failed", dbError);
-      throw dbError;
-    }
-    console.log("user 0", user);
-
-    if (!user) {
-      console.log("User not found");
-      return;
-    }
-    console.log("user", user);
-    console.log("eventAccountUpdated", eventAccountUpdated);
-    // update the account data
-    const t = await prisma.userProfile.update({
-      where: {
-        stripeAccountId: accountID,
-      } as any,
-      data: {
-        chargesEnabled: eventAccountUpdated.data.object.charges_enabled,
-        transfersEnabled: eventAccountUpdated.data.object.charges_enabled,
-        stripeDetailsSubmitted:
-          eventAccountUpdated.data.object.details_submitted,
-      },
-    });
-    console.log("Account Updated was successful!", eventAccountUpdated);
-  } catch (err) {
-    console.log("error", err);
-    throw err;
-  }
-};
+const handleAccountUpdated = async (event: Stripe.Event) => {};
 
 export async function POST(request: any) {
   try {
@@ -76,7 +27,7 @@ export async function POST(request: any) {
     // Handle the event
     switch (event.type) {
       case "account.updated":
-        handleAccountUpdated(event);
+        await handleAccountUpdated(event);
         break;
       // ... handle other event types
       default:
